d44b823f75066dc7010b603563ea6b7f8ce7fbd2
프리마 <fc1943s@pm.me>
2025-02-27 15:18:44 -0400

    tests

--- apps/documents/build.ps1
+++ apps/documents/build.ps1
@@ -12,7 +12,7 @@ $ErrorActionPreference = "Stop"
 $projectName = "documents"
 
 if (!$fast -and !$SkipNotebook) {
-    { . deps/spiral/workspace/target/release/spiral$(_exe) dib --path $ScriptDir/$projectName.dib } | Invoke-Block -Retries 3 -Location ../../../polyglot
+    { . deps/spiral/workspace/target/release/spiral$(_exe) dib --path "$ScriptDir/$projectName.dib" } | Invoke-Block -Retries 3 -Location ../../../polyglot
 }
 
 { . ../../deps/polyglot/apps/parser/dist/DibParser$(_exe) "$projectName.dib" spi } | Invoke-Block
