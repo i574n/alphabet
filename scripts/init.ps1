69cc3409336d60b7fdf1a5f2e864342bc69e2bdb
프리마 <fc1943s@pm.me>
2025-03-11 12:10:44 -0400

    tests

--- scripts/init.ps1
+++ scripts/init.ps1
@@ -26,8 +26,8 @@ EnsureSymbolicLink -Path "../deps/spiral" -Target "../../spiral"
 { pwsh ../deps/polyglot/deps/spiral/apps/spiral/build.ps1 -fast 1 -SkipFsx 1 } | Invoke-Block
 { pwsh ../deps/polyglot/apps/dir-tree-html/build.ps1 -fast 1 } | Invoke-Block
 
-{ sudo apt-get update } | Invoke-Block -Linux -Distro ubuntu
-{ sudo apt install -y texlive-xetex pandoc fonts-nanum-coding } | Invoke-Block -Linux -Distro ubuntu -OnError Continue
+{ apt-get update } | Invoke-Block -Linux -Distro ubuntu -OnError Continue
+{ apt install -y texlive-xetex pandoc fonts-nanum-coding } | Invoke-Block -Linux -Distro ubuntu -OnError Continue
 
 { cargo install --git https://github.com/crowdagger/crowbook.git --locked } | Invoke-Block -OnError Continue
 
