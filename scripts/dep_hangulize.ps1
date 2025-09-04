3a005beb28d877097a8de956410f105c1b18610e
Filipe Carvalho <filipesc@sigenius.com.br>
2025-09-04 16:40:20 -0400

    tests

--- scripts/dep_hangulize.ps1
+++ scripts/dep_hangulize.ps1
@@ -11,4 +11,14 @@ git clone --recurse-submodules https://github.com/i574n/hangulize.git
 Set-Location hangulize
 git pull
 
+if (!(Search-Command "go")) {
+    if ($IsWindows) {
+        winget install GoLang.Go --disable-interactivity --accept-source-agreements
+    }
+    else {
+        sudo apt update
+        sudo apt install -y golang
+    }
+}
+
 { go build -C cmd/hangulize } | Invoke-Block
