28b5b35a9cb86d63a5848508f5ee0754eb1125b3
프리마 <fc1943s@pm.me>
2025-10-26 14:06:06 -0400

    tests

--- scripts/dep_hangulize.ps1
+++ scripts/dep_hangulize.ps1
@@ -6,8 +6,12 @@ $ErrorActionPreference = "Stop"
 . ../deps/polyglot/scripts/core.ps1
 
 
+$url = git ls-remote --get-url
+$owner = ($url -split '/' | Select-Object -Last 2 | Select-Object -First 1) -replace '\.git$', '' ?? $env:GITHUB_REPOSITORY_OWNER
+Write-Output "dep_hangulize.ps1 / url: $url / owner: $owner"
+
 Set-Location (New-Item -ItemType Directory -Path "../deps" -Force)
-git clone --recurse-submodules https://github.com/i574n/hangulize.git
+git clone --recurse-submodules https://github.com/$owner/hangulize.git
 Set-Location hangulize
 git pull
 
