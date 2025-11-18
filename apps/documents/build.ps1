8e12479722520e0f7733fdd4df5370018c82a3ed
프리마 <fc1943s@pm.me>
2025-11-18 06:27:55 -0400

    tests

--- apps/documents/build.ps1
+++ apps/documents/build.ps1
@@ -15,7 +15,7 @@ if (!$fast -and !$SkipNotebook) {
     { . deps/spiral/workspace/target/release/spiral$(_exe) dib --path "$ScriptDir/$projectName.dib" } | Invoke-Block -Retries 3 -Location ../../../polyglot
 }
 
-{ . deps/spiral/workspace/target/release/spiral$(_exe) dib-export "$ScriptDir/$projectName.dib" spi } | Invoke-Block
+{ . ../../deps/spiral/workspace/target/release/spiral$(_exe) dib-export "$ScriptDir/$projectName.dib" spi } | Invoke-Block
 
 { . ../../deps/polyglot/apps/spiral/dist/Supervisor$(_exe) --build-file "$projectName.spi" "$projectName.fsx" --timeout 300000 } | Invoke-Block
 
